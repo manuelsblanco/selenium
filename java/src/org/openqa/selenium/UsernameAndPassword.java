6cccaa09d213e02bc8525b368c4eee6003803204
Manuel Blanco <mannywhite89@icloud.com>
2025-03-02 21:11:16 +0100

    Refactor UsernameAndPassword to use Java record
    Refactored the `UsernameAndPassword` class to a `record`, simplifying the implementation and ensuring immutability. Removed the redundant `of` method and `Supplier<Credentials>` usage, as they are no longer needed with the record structure.

--- java/src/org/openqa/selenium/UsernameAndPassword.java
+++ java/src/org/openqa/selenium/UsernameAndPassword.java
@@ -17,34 +17,13 @@
 
 package org.openqa.selenium;
 
-import java.util.function.Supplier;
 import org.openqa.selenium.internal.Require;
 
 /** A combination of username and password to use when authenticating a browser with a website. */
-public class UsernameAndPassword implements Credentials {
-
-  private final String username;
-  private final String password;
+public record UsernameAndPassword(String username, String password) implements Credentials {
 
   public UsernameAndPassword(String username, String password) {
     this.username = Require.nonNull("User name", username);
     this.password = Require.nonNull("Password", password);
-  }
-
-  public static Supplier<Credentials> of(String username, String password) {
-    Require.nonNull("User name", username);
-    Require.nonNull("Password", password);
-
-    Credentials creds = new UsernameAndPassword(username, password);
-
-    return () -> creds;
-  }
-
-  public String username() {
-    return username;
-  }
-
-  public String password() {
-    return password;
   }
 }
